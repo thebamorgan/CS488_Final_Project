fe6a77fa3218dd6c30e74a0b3c8cb2778bf7b5fe
Emma Fuller <eef0005@uah.edu>
2024-04-04 08:40:08 -0500

    Merged changes from last night

--- main.py
+++ main.py
@@ -15,20 +15,16 @@ df = pd.read_csv("bike_rental_data.csv")
 # PREPROCESSING
 # ******************************
 
-# Fix labels
-df['season'] = df['season'].map({1: 'spring', 2: 'summer', 3: 'fall', 4: 'winter'})
-df['weather'] = df['weather'].map({1: 'clear', 2: 'mist', 3: 'light rain', 4: 'heavy rain'})
-
 # Convert date time into a structure where it is easier to extract data
 df['datetime'] = pd.to_datetime(df['datetime'])
-# df['hour'] = df['datetime'].dt.hour
+df['hour'] = df['datetime'].dt.hour
+df.drop('datetime', axis=1, inplace=True)
 print(df.head())
 
 # Remove boolean values for pairplot because they aren't very helpful
 df_pair = df.copy()
 df_pair.drop('holiday', axis=1, inplace=True)
 df_pair.drop('workingday', axis=1, inplace=True)
-# print(df_pair.head())
 
 # Increase the number of colors in the palette
 num_colors = len(df_pair['season'].unique())  # Number of unique seasons
@@ -38,6 +34,7 @@ palette = sns.color_palette('bright', n_colors=num_colors)
 ax = sns.pairplot(df_pair, hue="season", height=2, aspect=0.7, palette=palette)
 plt.savefig('pair_plot_no_debug.png')
 # plt.show()
+
 # Following a website for this part:
 # https://www.analyticsvidhya.com/blog/2021/06/data-cleaning-using-pandas/
 
@@ -53,21 +50,19 @@ print("Sum:", df.duplicated().sum())  # Also zero
 # Let's look at the distribution of the data
 print(df.describe())
 
-# Since each column is distributed differently, let's compare some scaling methods
-
 # Normalization
-df_norm = df.copy()
-df_norm.drop('datetime', axis=1, inplace=True)
-df_norm.drop('season', axis=1, inplace=True)
-df_norm.drop('weather', axis=1, inplace=True)
 norm = MinMaxScaler()
-df_norm = pd.DataFrame(norm.fit_transform(df_norm))
-df_norm.columns = ['holiday', 'workingday', 'temp', 'atemp', 'humidity', 'windspeed', 'casual', 'registered', 'count']
-print(df_norm['workingday'])
+df_final = pd.DataFrame(norm.fit_transform(df))
+df_final.columns = ['season', 'holiday', 'workingday', 'weather', 'temp', 'atemp', 'humidity', 'windspeed', 'casual',
+                    'registered', 'count', 'hour']
+
+# Assigning labels for Season and Weather
+df_final['season'] = df_final['season'].map({0: 'spring', (1/3): 'summer', (2/3): 'fall', 1: 'winter'})
+df_final['weather'] = df_final['weather'].map({0: 'clear', (1/3): 'mist', (2/3): 'light rain', 1: 'heavy rain'})
 
 fig, axes = plt.subplots(1, 2, figsize=(20, 16))
-sns.boxplot(data=(df_norm['workingday'], df_norm['count']), ax=axes[0])
-sns.boxplot(data=(df_norm['holiday'], df_norm['count']), ax=axes[1])
+sns.boxplot(data=(df_final['workingday'], df_final['count']), ax=axes[0])
+sns.boxplot(data=(df_final['holiday'], df_final['count']), ax=axes[1])
 #sns.boxplot(data=df_norm, x='season', y='count', ax=axes[1, 0])
 #sns.boxplot(data=df_norm, x='weather', y='count', ax=axes[1, 1])
 plt.savefig('normalization.png')
